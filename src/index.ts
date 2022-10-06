d7ad35b08da9ae6b46cc5a84f664aa6cfafbe192
Coooookies <admin@mitay.net>
2022-10-06 22:46:31 +0800

    feat: demo

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,25 @@
  * @Author: Coooookies admin@mitay.net
  * @Date: 2022-10-04 19:38:35
  * @LastEditors: Coooookies admin@mitay.net
- * @LastEditTime: 2022-10-04 19:40:03
+ * @LastEditTime: 2022-10-06 22:43:30
  * @FilePath: \LeaugeMiddleware\src\index.ts
- * @Description: 
+ * @Description:
  */
-import './lcu/parser'
+
+import { LeagueClientManager } from './manager'
+
+const LCUManager = new LeagueClientManager()
+LCUManager.on('connected', (symbol, identificationm) => {
+  console.log('join', identificationm)
+})
+
+LCUManager.on('disconnected', (symbol, identificationm) => {
+  console.log('leave', identificationm)
+})
+
+LCUManager.on('message', (symbol, identificationm, message) => {
+  console.log('message', identificationm, message)
+})
+
+LCUManager.enableAutoConnect()
+console.log('Running')
