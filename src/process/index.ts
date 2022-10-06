17cb492760291381b0c0bd14fafa1c8ce3d65640
Coooookies <admin@mitay.net>
2022-10-06 22:46:22 +0800

    fix: 修复会弹出黑框框的BUG

--- src/process/index.ts
+++ src/process/index.ts
@@ -1,13 +1,25 @@
+/*
+ * @Author: Coooookies admin@mitay.net
+ * @Date: 2022-10-04 19:38:35
+ * @LastEditors: Coooookies admin@mitay.net
+ * @LastEditTime: 2022-10-06 21:45:10
+ * @FilePath: \LeaugeMiddleware\src\process\index.ts
+ * @Description:
+ */
 import os from 'os'
 import childProcess from 'child_process'
 import type { iProcessCase } from './types'
 
-// Windows only
-// Get process list from Tasklist
+const option = {
+  windowsHide: true
+}
+
+// Windows 专属
+// 从tasklist获取进程信息
 function getProcessList() {
   const command = 'tasklist'
   return new Promise<iProcessCase[]>((resolve, reject) => {
-    childProcess.exec(command, (err, stdout, stderr) => {
+    childProcess.exec(command, option, (err, stdout, stderr) => {
       if (err || !stdout || stderr) {
         return reject(err || stderr)
       }
@@ -27,11 +39,11 @@ function getProcessList() {
   })
 }
 
-// Get process from WMIC
+// 从wmic获取进程信息
 function getProcessFromNameA(name: string) {
   const command = `WMIC PROCESS WHERE name='${name}' GET name,processid`
   return new Promise<iProcessCase[]>((resolve, reject) => {
-    childProcess.exec(command, (err, stdout, stderr) => {
+    childProcess.exec(command, option, (err, stdout, stderr) => {
       if (err || !stdout || stderr) {
         return reject(err || stderr)
       }
@@ -53,7 +65,7 @@ function getProcessFromNameA(name: string) {
   })
 }
 
-// Get process
+// 通过进程名获取进程信息
 function getProcessFromName(name: string) {
   return new Promise<iProcessCase[]>((resolve, reject) =>
     getProcessList()
@@ -64,11 +76,11 @@ function getProcessFromName(name: string) {
   )
 }
 
-// Get process commandline from WMIC
+// 从wmic获取进程命令行
 function getCommandlineFromPid(processId: number) {
   const command = `WMIC PROCESS WHERE processid=${processId} GET commandline`
   return new Promise<string>((resolve, reject) => {
-    childProcess.exec(command, (err, stdout, stderr) => {
+    childProcess.exec(command, option, (err, stdout, stderr) => {
       if (err || !stdout || stderr) {
         return reject(err || stderr)
       }
