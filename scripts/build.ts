49bbc49a05aa4913ff844f04199aa8768bc6b210
Coooookies <admin@mitay.net>
2022-10-06 23:49:20 +0800

    feat: 新增打包exe的能力

---
+++ scripts/build.ts
@@ -0,0 +1,35 @@
+/*
+ * @Author: Coooookies admin@mitay.net
+ * @Date: 2022-10-06 23:22:41
+ * @LastEditors: Coooookies admin@mitay.net
+ * @LastEditTime: 2022-10-06 23:48:24
+ * @FilePath: \LeaugeMiddleware\scripts\build.ts
+ * @Description:
+ */
+import exe from '@angablue/exe'
+import path from 'path'
+import config from '../package.json'
+
+const build = exe({
+  entry: './build/index.js',
+  out: `./dist/${config.name}.exe`,
+  pkg: ['-C', 'GZip'],
+  version: config.version,
+  target: 'node16-windows-x64',
+  //   icon: './assets/icon.ico',
+  properties: {
+    FileDescription: config.description,
+    ProductName: config.productName,
+    LegalCopyright: config.copyright,
+    CompanyName: config.author
+  }
+})
+
+build.then(() => {
+  console.log('==========================')
+  console.log('Build completed!')
+  console.log(
+    `Output: ${path.resolve(__dirname, `../dist/${config.name}.exe`)}`
+  )
+  console.log('==========================')
+})
